37394184dba26893982b9e2a1cd587ff7cffa47d
jherd801 <jherd801@gmail.com>
2020-07-31 13:49:47 -0600

    Created ProfileCard component to display user profile information on dashboard screen

---
+++ client/src/components/Listing/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyListing() {
+    return (
+        <div className="Container">
+            PLACEHOLDER FOR A CARD TO HOLD CURRENT LISTING INFORMATION
+        </div>
+    )
+}
+
+export default MyListing;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import './style.css';
 function Navbar() {
     return (
         <nav className="navbar navbar-light bg-dark">
-            <span className="navbar-brand mb-0 h1 text-dark navlink"><Link to="/">Home</Link></span>
+            <span className="navbar-brand mb-0 h1 text-dark navlink"><Link to="/">Dashboard</Link></span>
             <span className="navbar-brand mb-0 h1 text-dark navlink"><Link to="/buysell">Buy Sell</Link></span>
             <span className="navbar-brand mb-0 h1 text-dark navlink"><Link to="/listings">Listings</Link></span>
             <span className="navbar-brand mb-0 h1 text-dark navlink"><Link to="/login">Login</Link></span>
