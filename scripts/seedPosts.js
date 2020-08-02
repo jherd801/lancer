2e4fa17d1c0f899878556e704d5163f88fee56d8
jherd801 <jherd801@gmail.com>
2020-08-02 15:23:10 -0600

    Added base database seed files to scripts directory

---
+++ scripts/seedPosts.js
@@ -0,0 +1,46 @@
+// const mongoose = require("mongoose");
+// const db = require("../models");
+
+// mongoose.connect(
+//   process.env.MONGODB_URI ||
+//   "mongodb://localhost/reactreadinglist"
+// );
+
+const postsSeed = [
+    {
+        title: "",
+        description: "",
+        price: "",
+        category: ""
+    },
+    {
+        title: "",
+        description: "",
+        price: "",
+        category: ""
+    },
+    {
+        title: "",
+        description: "",
+        price: "",
+        category: ""
+    },
+    {
+        title: "",
+        description: "",
+        price: "",
+        category: ""
+    },
+    {
+        title: "",
+        description: "",
+        price: "",
+        category: ""
+    },
+    {
+        title: "",
+        description: "",
+        price: "",
+        category: ""
+    }
+]
---
+++ scripts/seedUsers.js
@@ -0,0 +1,73 @@
+// const mongoose = require("mongoose");
+// const db = require("../models");
+
+// mongoose.connect(
+//   process.env.MONGODB_URI ||
+//   "mongodb://localhost/reactreadinglist"
+// );
+
+const usersSeed = [
+    {
+        firstName: "Michael",
+        lastName: "Scott",
+        email: "michael@email.com",
+        password: "password",
+        city: "Scranton",
+        state: "Pennsylvania",
+        zipcode: "18503"
+    },
+    {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        city: "",
+        state: "",
+        zipcode: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        city: "",
+        state: "",
+        zipcode: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        city: "",
+        state: "",
+        zipcode: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        city: "",
+        state: "",
+        zipcode: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        city: "",
+        state: "",
+        zipcode: ""
+    },
+    {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        city: "",
+        state: "",
+        zipcode: ""
+    }
+]
