82568272bfd11f471a6f0667c3fd7c124d9f987f
Ashton Walden <ashtonwalden@icloud.com>
2020-08-03 08:56:00 -0600

    updated server.js to use mongodb

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
-var express = require("express");
-var PORT = process.env.PORT || 8000;
-var app = express();
+// var express = require("express");
+// var PORT = process.env.PORT || 8000;
+// var app = express();
 
-// app.use(express.static("public"));
+// // app.use(express.static("public"));
 
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// if (process.env.NODE_ENV === "production") {
+//     // not exactly sure where the "/build" comes from
+//     app.use(express.static("client/build"))
+// };
+
+// // may need to double check on pathing / file names
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+// app.listen(PORT, () => {
+//     console.log("Listening on Port: " + PORT)
+// });
+
+
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+// routes => react
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 if (process.env.NODE_ENV === "production") {
-    // not exactly sure where the "/build" comes from
-    app.use(express.static("client/build"))
-};
+    // serves up static assests on heroku 
+    app.use(express.static("client/build"));
+}
 
-// may need to double check on pathing / file names
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// use routes (API may still need. Not sure on react.) => heroku
 
-app.listen(PORT, () => {
-    console.log("Listening on Port: " + PORT)
-});
+// connnects to mongo databse
+    // may need to change route based on front end
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lancerServices");
+
+// API server starts listening
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
