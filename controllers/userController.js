376c7cc6b7587a06b09d3c437c0f3f3aeb76f96e
Ashton Walden <ashtonwalden@icloud.com>
2020-08-03 14:26:52 -0600

    added userController.js file

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const db = require("../models/user");
+
+module.exports = {
+    // create funciton for creating new user
+    create: function(req, res) {
+        db.User
+            .create(req.body)
+            // .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+
+    // update function for changing user
+    update: function(req, res) {
+        db.User
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    // delete function for deleting a user
+    remove: function(req, res) {
+        db.User
+          .findById({ _id: req.params.id })
+          .then(dbModel => dbModel.remove())
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+    }
+}
---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema({
+    username: { type: String, required: true },
+    // anyway to verify it is in email format ("@" & ".")?
+    email: { type: String, required: true },
+    // hashing password - ( https://stackoverflow.com/questions/14588032/mongoose-password-hashing )
+    // Other ways to hide password?
+    password: { type: String, required: true }
+    // location ? (what type and how would we get it)
+});
+
+const User = mongoose.model("User", usersSchema);
+
+module.exports = User;
