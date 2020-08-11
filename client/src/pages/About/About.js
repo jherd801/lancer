caa42332e7615bed7dff31c98add3529a45f86a5
kingaman99 <aman.bedi26@gmail.com>
2020-08-10 19:47:16 -0600

    updated about page

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -7,7 +7,11 @@ const About = () => (
     <Navbar />
     <h1>
       About Page
-    </h1>    
+    </h1>   
+    <p>Lancer was designed with the intention of giving freelance and self-employed individuals a platform to display their skills, as well as providing users a fast, easy, and comprehensive way of searching for and hiring individuals in their area.
+      It also gives everyone a chance to help out there local businesses in their city and state. 
+      We try to help self-employed individuals and freelancers to build their own clientele and also to gain experiences for jobs if they ever do decide they would like to work for a company in the future.
+    </p>
   </div>
 );
 
