548910860d0ab71c243603cbe9fb0eb28d670063
jherd801 <jherd801@gmail.com>
2020-08-12 18:12:33 -0600

    sdfjhosfh

---
+++ client/build/precache-manifest.4c22d93c6b652153b7a4faf1967ea814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f008cd0d4558d9af731313a0a24e841e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "025760cab637fa8036ac",
+    "url": "/static/css/main.248c34e9.chunk.css"
+  },
+  {
+    "revision": "c63201c39a66c182ffcf",
+    "url": "/static/js/2.70e691af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.70e691af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025760cab637fa8036ac",
+    "url": "/static/js/main.91b8b068.chunk.js"
+  },
+  {
+    "revision": "b1831c7605bdc54b26d2",
+    "url": "/static/js/runtime-main.f7d130a4.js"
+  }
+]);
